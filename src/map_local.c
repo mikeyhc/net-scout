40b44ac705d473f567157817a845907b5ca84074
mikeyhc <blockley.mike@gmail.com>
2013-11-01 19:57:28 +1300

    added application to call functions

---
+++ src/map_local.c
@@ -0,0 +1,11 @@
+/* map_local.c
+ * small application for testing local function
+ *
+ * author: mikeyhc <mikeyhc@atmosia.net>
+ */
+
+#include <netscout.h>
+
+int main(int *argc, char **argv){
+	map_local_network();
+}/* end: main */
