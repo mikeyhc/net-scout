062706d64a1f3708dec8fbfd293726372830da12
mikeyhc <blockley.mike@gmail.com>
2013-11-02 02:22:02 +1300

    ping added, not functional

--- src/netscout-common.c
+++ src/netscout-common.c
@@ -6,12 +6,16 @@
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 #include <netdb.h>
 #include <ifaddrs.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+#define BUFFER_SIZE 400
 
 static struct ifaddrs *ifaddr = NULL;		/* store for single fetch */
 
@@ -87,6 +91,81 @@ increase_sockaddr(struct sockaddr *a, int family, int increase){
 		}
 }/* end: increase_sockaddr */
 
+/* testping: sends a ping from a given source address to a given host address
+ * and will prrint a message if there is a reply
+ *
+ * param src: the address to send from
+ * param dst: the address to send to
+ */
+static void
+testping(struct sockaddr *src, struct sockaddr *dst){
+	int s, on, dstlen;
+	struct ip *ip;
+	struct icmphdr *icmp;
+	char buf[BUFFER_SIZE];
+
+	ip = (struct ip*)buf;
+	icmp = (struct icmphdr*)(ip + 1);
+	memset(buf, 0, BUFFER_SIZE);
+
+	if((s = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) , 0){
+		perror("socket() error");
+		exit(1);
+	}
+
+	on = 1;
+	if(setsockopt(s, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on)) < 0){
+		perror("setsockopt() for BROADCAST error");
+		exit(EXIT_FAILURE);
+	}
+
+	if(setsockopt(s, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) < 0){
+		perror("setsockopt() for IP_HDRINCL error");
+		exit(EXIT_FAILURE);
+	}
+
+	ip->ip_v = 4;
+	ip->ip_hl = sizeof*ip >> 2;
+	ip->ip_tos = 0;
+	ip->ip_len = htons(sizeof(buf));
+	ip->ip_id = htons(4321);
+	ip->ip_off = htons(0);
+	ip->ip_ttl = 255;
+	ip->ip_p = 1;
+	ip->ip_sum = 0; /* let the kernel do its thang */
+	
+	icmp->type = ICMP_ECHO;
+	icmp->code = 0;
+	icmp->checksum = htons(~(ICMP_ECHO << 8));
+
+	ip->ip_src.s_addr = 0x00000000;
+	ip->ip_src.s_addr |= (src->sa_data[5] & 0xFF) << 24;
+	ip->ip_src.s_addr |= (src->sa_data[4] & 0xFF) << 16;
+	ip->ip_src.s_addr |= (src->sa_data[3] & 0xFF) << 8;
+	ip->ip_src.s_addr |= (src->sa_data[2] & 0xFF);
+
+	ip->ip_dst.s_addr = 0x00000000;
+	//ip->ip_dst.s_addr |= (dst->sa_data[5] & 0xFF) << 24;
+	ip->ip_dst.s_addr |= (2 & 0xFF) << 24;
+	ip->ip_dst.s_addr |= (dst->sa_data[4] & 0xFF) << 16;
+	ip->ip_dst.s_addr |= (dst->sa_data[3] & 0xFF) << 8;
+	ip->ip_dst.s_addr |= (dst->sa_data[2] & 0xFF);
+
+	printf("dst: %s\n", inet_ntoa(ip->ip_dst));
+	dstlen = sizeof(*dst);
+	if(sendto(s, buf, sizeof(buf), 0, dst, dstlen) < 0){
+		perror("error with sendto()");
+		exit(EXIT_FAILURE);
+	}
+
+	if(recvfrom(s, buf, sizeof(buf), 0, dst, &dstlen) < 0){
+		perror("error with recvfrom()");
+		exit(EXIT_FAILURE);
+	}
+
+	close(s);
+}/* end: testping */
+
 /* map_local_network: will get all the interfaces on this machine, then get the
  * network and netmask for each interface, using that it will attempt to ping 
  * every possible host in each network and print the results.
@@ -161,15 +240,7 @@ map_local_network(void){
 		sockaddrcpy(&sa_temp, &net, family);
 		increase_sockaddr(&sa_temp, family, 1);
 		while(compare_sockaddr(&broad, &sa_temp, family)<0){
-			s = getnameinfo(&sa_temp,
-					(family == AF_INET) ? sizeof(struct sockaddr_in) :
-																sizeof(struct sockaddr_in6),
-					host_temp, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-			if(s!=0){
-				printf("getnameinfo() failed: %s\n", gai_strerror(s));
-				exit(EXIT_FAILURE);
-			}
-			printf("testing %s: \n", host_temp);
+			testping(ifa->ifa_addr, &sa_temp);
 			increase_sockaddr(&sa_temp, family, 1);
 		}
 	}
